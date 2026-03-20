99547b5e3099cc6849ed095a262a2d0a1d3d6bac
Shivanand Gupta <142976206+Shivanand-0@users.noreply.github.com>
2026-03-20 15:59:34 +0530

    Refactor CORS and app run configuration
    Updated CORS configuration and modified app run settings.

--- mlModel/index.py
+++ mlModel/index.py
@@ -5,12 +5,12 @@ from joblib import load
 import numpy as np 
 
 app=Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "http://127.0.0.1:5173"}})
+CORS(app)
 model=load('./mushrooms_model_pickle')
 scaler = load('./scaler_object')
 
 @app.route('/predict', methods=['POST'])
-@cross_origin(origin='http://127.0.0.1:5173')
+@cross_origin()
 def predict():
     data= request.get_json()
     features_name=['cap-shape', 'cap-surface','gill-attachment', 'gill-size','veil-color', 'spore-print-color', 'population', 'habitat' ]
@@ -18,9 +18,11 @@ def predict():
     for i,feature in enumerate(features_name):
         features.append(data[feature])
     features=np.array(features).reshape(1,-1)
+    scaled_features = scaler.transform(features)
     prediction=model.predict(features)
     return jsonify({'prediction':prediction.tolist()})
 
 
 if __name__=='__main__':
-    app.run(port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
